7d8d5037132a515a42c2b40e53672f228a50d385
Rosa <rosa.m.tung@gmail.com>
2019-01-19 14:17:04 -0500

    parse csv file

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,25 @@
 import React, { Component } from "react";
 import { hot } from "react-hot-loader";
 import "./App.scss";
+import Papa from "papaparse";
 
 import UploadFile from "./UploadFile";
 
 class App extends Component {
-   _uploadFile(file) {
-      console.log(file);
+   _parseFile(file) {
+      console.log("file: ", file);
+      Papa.parse(file, {
+         complete: (results) => {
+            console.log("results: ", results);
+         }
+      });
    }
 
    render() {
       return (
          <div className="App">
             <UploadFile
-               onChangeAction={this._uploadFile}
+               onChangeAction={this._parseFile}
             />
          </div>
       );
