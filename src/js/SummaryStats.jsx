de227f0d5bcc1ee5cb2a570f41866e34a88328b9
Rosa <rosa.m.tung@gmail.com>
2019-01-20 17:15:29 -0500

    start grid logic

--- src/js/SummaryStats.jsx
+++ src/js/SummaryStats.jsx
@@ -1,28 +1,47 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 
-import "../sass/SummaryStats.scss";
-
 class SummaryStats extends Component {
    constructor(props) {
       super(props);
 
       this.state = {
-         bodyData: []
+			grid: [],
+			template: []
       };
    }
 
    componentDidMount() {
-      this.setState({ bodyData: this.props.bodyData });
+		
+      this.setState({ grid: this.props.bodyData });
    }
 
    componentDidUpdate(prevProps) {
       if (this.props.bodyData !== prevProps.bodyData) {
-         this.setState({ bodyData: this.props.bodyData });
+         this.setState({ grid: this.props.bodyData });
       }
    }
 
+	_calculateTemplate(_bodyData) {
+		
+	}
+
+	_calculateGrid(_bodyData) {
+		const _template = this._calculateRowTemplate(); //[1, 3, 5]
+		const _dataByColumns = [];
+
+		//go through each row
+		this.props.bodyData.forEach((row, x) => {
+			//go through each column
+			row.forEach((cell, y) => {
+				//if index is one we care about
+				//array[i].push(row[i])
+			});
+		});
+	}
+
    _renderMean() {
+		//traverse through array
       return (
          <tr>
             <td>hello</td>
@@ -47,13 +66,26 @@ class SummaryStats extends Component {
    }
 
    render() {
-      return (
-         <tfoot>
-            {this._renderMean()}
-            {this._renderMedian()}
-            {this._renderMode()}
-         </tfoot>
-      );
+		const _grid = this.state.grid;
+
+		if(_grid && _grid.length > 0) {
+			return (
+				<tfoot>
+					{this._renderMean()}
+					{this._renderMedian()}
+					{this._renderMode()}
+				</tfoot>
+			);
+		} else {
+			return (
+				<tfoot>
+					<tr>
+						No Number Columns
+					</tr>
+				</tfoot>
+			);
+		}
+
    }
 }
 
